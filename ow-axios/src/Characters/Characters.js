c0df7a46bfb953c98941811c12d64a6c8cdd740d
SeanMee86 <seanmee8186@gmail.com>
2019-10-26 10:24:06 -0700

    finished load character functionality

--- ow-axios/src/Characters/Characters.js
+++ ow-axios/src/Characters/Characters.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './Characters.css';
 
 const characters = props => {
+    const style = {
+        width: "150px"
+    };
     return (
-        <div onClick={() => props.click(props.hero)} className={'character'}>
+        <div style={!props.actionToTake ? style : null} onClick={() => props.actionToTake ? props.loadSingleChar(props.hero) : props.loadAllChars()} className={'character'}>
             <img src={`https://d1u1mce87gyfbn.cloudfront.net/hero/${props.content}/hero-select-portrait.png`} alt=""/>
         </div>
     )
