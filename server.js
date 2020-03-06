1c765a74e698a8b0721699977d24fd62bcf7ad76
hss-smee <seanmee8186@gmail.com>
2020-03-06 10:22:51 -0800

    finished deployment to heroku

--- server.js
+++ server.js
@@ -89,9 +89,9 @@ app.get('/api/v1/hero', (req, res) => {
 });
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/build'));
+    app.use(express.static(path.join(__dirname, '/client/build')));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
