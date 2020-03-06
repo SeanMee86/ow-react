254847bdc9b9da7375e9a5454dc5037c5f238865
SeanMee86 <sean.mee86@yahoo.com>
2020-03-06 07:07:07 -0800

    preparing for heroku

--- server.js
+++ server.js
@@ -89,10 +89,10 @@ app.get('/api/v1/hero', (req, res) => {
 });
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/build'));
+    app.use(express.static('./client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, './client/build/index.html'))
     })
 }
 
